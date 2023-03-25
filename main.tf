eb7696f52d5627d4afe0441033ed2f64b1a5ac72
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 22:31:44 +0530

    updated rabbitmq

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "aws_spot_instance_request" "rabbitmq" {
   subnet_id            = var.subnet_ids[0]
   wait_for_fulfillment = true
 
-
   tags = merge(
     var.tags,
     { Name = "${var.env}-rabbitmq" }
