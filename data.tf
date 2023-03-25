eaf539cb33150f7e77d2d7100ce2d541e95224bf
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 23:02:41 +0530

    updated rabbitmq

--- data.tf
+++ data.tf
@@ -1,5 +1,11 @@
-data "aws_ami" "ami" {
+data "aws_ami" "ami_id" {
   most_recent = true
   name_regex = "Devops-ansible-practice"
-  owners = ["self"]
+  owners = "875386077775"
 }
+
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex = "Devops-ansible-practice"
+#  owners = "875386077775"
+#}
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "rabbitmq" {
-  ami = "ami-01f00f562868293a1"
+  ami = data.aws_ami.ami_id
   instance_type = var.instance_type
   subnet_id = var.subnet_ids[0]
   wait_for_fulfillment = true
