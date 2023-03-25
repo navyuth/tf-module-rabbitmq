9c9c5e5f145af3e3c26388ca75ea99d33a30e774
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 23:23:57 +0530

    updated rabbitmq

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "rabbitmq" {
-  ami = data.aws_ami.ami.id
+  ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   subnet_id = var.subnet_ids[0]
   wait_for_fulfillment = true
