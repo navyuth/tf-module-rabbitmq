0509ba8252670b5b10dead37273cd997596e4752
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-04-02 12:44:01 +0530

    updated rabbitmq for payment service

--- data.tf
+++ data.tf
@@ -7,3 +7,8 @@ data "aws_ami" "ami" {
 
 }
 
+data "aws_route53_zone" "domain" {
+  name = var.dns_domain
+}
+
+data "aws_caller_identity" "account" {}
--- vars.tf
+++ vars.tf
@@ -2,3 +2,10 @@ variable "instance_type" {}
 variable "tags" {}
 variable "env" {}
 variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
+variable "dns_domain" {}
+variable "component" {
+  default = "rabbitmq"
+}
+variable "bastion_cidr" {}
